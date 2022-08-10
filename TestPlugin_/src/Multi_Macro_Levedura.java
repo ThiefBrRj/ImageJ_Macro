8bb5c4e925c8c7804d476bb7f4791d6ba2abf2dd
Thiago Fagundes <99626328+ThiefBrRj@users.noreply.github.com>
2022-08-09 22:41:34 -0300

    Add files via upload

--- TestPlugin_/src/Multi_Macro_Levedura.java
+++ TestPlugin_/src/Multi_Macro_Levedura.java
@@ -1,4 +1,5 @@
 import java.awt.Rectangle;
+import java.io.File;
 
 import ij.IJ;
 import ij.ImagePlus;
@@ -7,25 +8,33 @@ import ij.plugin.PlugIn;
 public class Multi_Macro_Levedura implements PlugIn {
 	
 	public void run(String arg) {
-		ImagePlus imagem = IJ.getImage();
 		
-		imagem.setRoi(558,354,2088,1836);
-		imagem = imagem.crop();
-		ImagePlus imagemCopy = imagem.duplicate();
-				
-		try {
-			IJ.run(imagemCopy, "8-bit", "");
-			IJ.run(imagemCopy, "Median...", "radius=18");
-			IJ.setThreshold(imagemCopy, 155, 255);
-			IJ.run(imagemCopy, "Convert to Mask", "");
-			IJ.doWand(imagemCopy, imagemCopy.getWidth()/2, imagemCopy.getHeight()/2, 0.0, "Legacy");
-			Rectangle bounds = imagemCopy.getRoi().getBounds();
-			imagem.setRoi(bounds);
-		} catch (NullPointerException e) {
-			e.printStackTrace();
+		File pasta = new File(IJ.getDirectory("Origem"));
+		String destino = IJ.getDirectory("Destino");
+		String[] imagens = pasta.list();
+		
+		for (int controle = 0; controle < imagens.length; controle++) {
+			ImagePlus imagem = IJ.openImage(pasta + "\\" + imagens[controle]);
+			String imagemNome = imagem.getShortTitle();
+			
+			imagem.setRoi(558,354,2088,1836);
+			imagem = imagem.crop();
+			ImagePlus imagemCopy = imagem.duplicate();
+					
+			try {
+				IJ.run(imagemCopy, "8-bit", "");
+				IJ.run(imagemCopy, "Median...", "radius=18");
+				IJ.setThreshold(imagemCopy, 155, 255);
+				IJ.run(imagemCopy, "Convert to Mask", "");
+				IJ.doWand(imagemCopy, imagemCopy.getWidth()/2, imagemCopy.getHeight()/2, 0.0, "Legacy");
+				Rectangle bounds = imagemCopy.getRoi().getBounds();
+				imagem.setRoi(bounds);
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+			}
+			
+			imagem = imagem.crop();
+			IJ.saveAs(imagem, "tif", destino + imagemNome + "-croped");
 		}
-		
-		imagem = imagem.crop();
-		imagem.show();
 	}
 }
